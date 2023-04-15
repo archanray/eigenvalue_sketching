9841792e8ef0005685c247a49579c3cf3a8bb9d8
unknown <talk2archan@gmail.com>
2023-04-15 14:21:22 -0400

    construct

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import numpy as np
-
+from src.approximator import eigval_approx_bki_adaptive as bki_adp
+from src.approximator import eigval_approx_othro_adaptive as oth_adp
+from src.approximator import eigval_approx_ortho_nonadaptive as oth_nonadp
+from src.approximator import eigval_approx_SW_nonadaptive as sw_nonadp
+from src.datasets import load_dataset
