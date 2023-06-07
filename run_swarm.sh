9841f3100d979b60427652b7e31ac6c72a1ab023
Archan Ray <ray@swarm.cs.umass.edu>
2023-06-07 06:35:45 -0400

    updated bash

--- run_swarm.sh
+++ run_swarm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # sbatch -J sweep_eps -o sweep_eps.out -e sweep_eps.err --nodes 6 --cpus-per-task 12 -p longq --mem 32000 run_swarm.sh
 
-python single_function_tester.py 
+#python single_function_tester.py 
+python main_matvecs.py
