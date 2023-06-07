828e42ac41523a924fef28b7e0b20826548e0ad5
archanray <ray@cs.umass.edu>
2023-06-07 16:15:02 +0530

    sys inputs

--- run_swarm.sh
+++ run_swarm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sbatch -J sweep_eps -o sweep_eps.out -e sweep_eps.err --nodes 6 --cpus-per-task 12 -p longq --mem 32000 run_swarm.sh
+# sbatch -J dataset_name -o dataset_name.out -e dataset_name.err --nodes 6 --cpus-per-task 12 -p longq --mem 32000 run_swarm.sh dataset_name
 
 #python single_function_tester.py 
 python main_matvecs.py $1
