05dab4d6607e34a73b97e2932419802ffbc352b9
archanray <ray@cs.umass.edu>
2023-06-07 15:52:57 +0530

    multiple trials

--- main_plotter.py
+++ main_plotter.py
@@ -5,11 +5,15 @@ import os
 
 dataset_name = "random_random"
 
-def sorter(xvals, yvals):
+def sorter(xvals, y1vals, y2vals=None):
 	ids = np.argsort(xvals)
 	xvals = xvals[ids]
-	yvals = yvals[ids]
-	return xvals, yvals
+	y1vals = y1vals[ids]
+	if y2vals is None:
+		return xvals, y1vals
+	else:
+		y2vals = y2vals[ids]
+		return xvals, y1vals, y2vals
 
 ########################## load data #######################
 with open("results/"+dataset_name+".pkl", "rb") as f:
@@ -22,11 +26,14 @@ for i in save_vars["params"]["sr"]:
 	print(i)
 	plt.gcf().clf()
 	for mthd in save_vars["save_vals"].keys():
-		plt.plot(np.log(save_vars["save_vals"][mthd][4]), \
-			save_vars["save_vals"][mthd][0][:,i], label=mthd)
-		plt.fill_between(np.log(save_vars["save_vals"][mthd][4]), \
-			save_vars["save_vals"][mthd][2][:,i], \
-			save_vars["save_vals"][mthd][3][:,i], alpha=0.2)
+		xvals, yvals = sorter(np.log(save_vars["save_vals"][mthd][4]), \
+								save_vars["save_vals"][mthd][0][:,i])
+		plt.plot(xvals, yvals, label=mthd)
+
+		xvals, y1vals, y2vals = sorter(np.log(save_vars["save_vals"][mthd][4]), \
+								save_vars["save_vals"][mthd][2][:,i], \
+								save_vars["save_vals"][mthd][3][:,i])
+		plt.fill_between(xvals, y1vals, y2vals, alpha=0.2)
 	plt.xlabel("log matvecs")
 	plt.ylabel("log absolute errors")
 	plt.legend()
