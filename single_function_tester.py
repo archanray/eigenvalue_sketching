33685a23b831d594bd10c11ac9c0fe05cb48b7ab
archanray <ray@cs.umass.edu>
2023-07-03 19:52:37 +0530

    facebook test single bki

--- single_function_tester.py
+++ single_function_tester.py
@@ -54,7 +54,7 @@ for j in tqdm(range(len(c)), position=0):
 		for t in range(trials): 
 			# alpha = eigval_approx(A, k_now, c[j]) # these are the approximate eigvals at each round
 			alpha, _ = bki(A, k=k, k_given=True, \
-                                        q=c, q_given=True, mode="Q", sr=[])
+                                        q=c[j], q_given=True, mode="Q", sr=[])
 			errors[t,:] = lambda_A - alpha # error at a single round
 
 		avg_errors[i,:] = np.log(np.abs(np.mean(errors, axis=0)))
